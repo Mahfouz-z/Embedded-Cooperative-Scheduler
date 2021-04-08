f134c1657b1374cd5c27ace75e7b80665fa1fea5
hysamah <57324118+hysamah@users.noreply.github.com>
2021-04-08 18:02:41 +0200

    API implementation

--- co_sched.c
+++ co_sched.c
@@ -5,33 +5,97 @@
 
 struct Qnode * readyQ_root;
 struct Qnode * delayedQ_root;
+struct Qnode * running;
+
 
 
 int Init()
 {
-    int error = 0;
+    int state = 0;
     readyQ_root = NULL; 
     delayedQ_root = NULL;
-    return error;
+    return state;
 }
 int QueTask(void (*task_ptr), unsigned int priority)
 {
-    int error = 0;
+    int state = 0;
+    struct Qnode * node = (struct Qnode *) malloc(sizeof(struct Qnode));
+    struct Qnode * itr = readyQ_root->next;
+
+    if (node != NULL)
+    {
+        if (itr != NULL)
+        {    while (itr->next->priority <= priority)
+            {
+                itr=itr->next;
+                if (itr->next == NULL) break;
+
+            }
+        }
+        
+        node -> priority = priority;
+        node -> task_ptr = task_ptr;
+        node -> next = itr->next;
+        node->sleep_time = 0;
+        if (itr == NULL) itr = node;
+        else itr->next = node;
+    }
+    else state = -1; //memory allocation error
+    return state;
     
-    return error;
 }
 
 int ReRunME(int delay)
 {
-    int error = 0;
+    int state = 0;
+    if (delay == 0) 
+    {
+        state = QueTask(running->task_ptr, running->priority);
+        free(running);
+    }
+    else 
+    {
+        struct Qnode * node = (struct Qnode *) malloc(sizeof(struct Qnode));
+        struct Qnode * itr = delayedQ_root->next;
 
-    return error;
+        if (node != NULL)
+        {
+            if (itr != NULL)
+            {    while (itr->next->sleep_time <= delay)
+                {
+                    itr=itr->next;
+                    if (itr->next == NULL) break;
+
+                }
+            }
+            
+            node -> priority = running->priority;
+            node -> task_ptr = running->task_ptr;
+            node -> next = itr->next;
+            node->sleep_time = delay;
+            if (itr == NULL) itr = node;
+            else itr->next = node;
+            free(running);
+        }
+        else state = -1; //memory allocation error
+    }
+
+    return state;
 }
 
 int Dispatch()
 {
-    int error = 0;
+    int state = 0;
+    if (readyQ_root != NULL)
+    {
+        running = readyQ_root;
+        readyQ_root = readyQ_root->next;
+        ((void(*)())running->task_ptr)();
+        free(running);
 
-    return error;
+    } 
+    else state = 1; //idle
+
+    return state;
 }
 
