19a8d5be7a374727bece5c167ab3caa5bd9fb12a
hysamah <57324118+hysamah@users.noreply.github.com>
2021-04-10 11:58:15 +0200

    Modified scheduler

--- co_sched.c
+++ co_sched.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "co_sched.h"
 #include "co_sched_API.h"
 
-struct Qnode {
-    int priority;
-    int sleep_time;
-    void (*task_ptr);
-    struct Qnode* next;
-};
 
-struct Qnode * readyQ_root;
-struct Qnode * delayedQ_root;
-struct Qnode * running;
+ struct Qnode * readyQ_root;
+ struct Qnode * delayedQ_root;
+ struct Qnode * running;
 
 
 
@@ -19,31 +14,36 @@ int Init()
 {
     int state = 0;
     readyQ_root = NULL; 
+		readyQ_root -> next ->priority = 15;
     delayedQ_root = NULL;
     return state;
 }
-int QueTask(void (*task_ptr), unsigned int priority)
+int QueTask(volatile void (*task_ptr), unsigned int priority)
 {
     int state = 0;
     struct Qnode * node = (struct Qnode *) malloc(sizeof(struct Qnode));
     struct Qnode * itr = readyQ_root;
 
+
     if (node != NULL)
     {
-        if (itr != NULL)
-        {    while (itr->next->priority <= priority)
-            {
-                itr=itr->next;
-                if (itr->next == NULL) break;
-
+      if (itr != NULL)  
+			if (itr ->priority < 8)
+        {    
+						while (itr->next->priority <= priority)
+							{
+									itr=itr->next;
+									if (itr->next == NULL) 
+										break;
             }
+					
         }
         
         node -> priority = priority;
         node -> task_ptr = task_ptr;
         node -> next = itr->next;
         node->sleep_time = 0;
-        if (itr == NULL) readyQ_root = node;
+        if (itr ->priority >= 8) readyQ_root = node;
         else itr->next = node;
     }
     else state = -1; //memory allocation error
@@ -57,7 +57,6 @@ int ReRunME(int delay)
     if (delay == 0) 
     {
         state = QueTask(running->task_ptr, running->priority);
-        free(running);
     }
     else 
     {
@@ -66,7 +65,8 @@ int ReRunME(int delay)
 
         if (node != NULL)
         {
-            if (itr != NULL)
+					if (itr != NULL)
+            if (itr ->priority < 8)
             {    while (itr->next->sleep_time <= delay)
                 {
                     itr=itr->next;
@@ -79,23 +79,32 @@ int ReRunME(int delay)
             node -> task_ptr = running->task_ptr;
             node -> next = itr->next;
             node->sleep_time = delay;
-            if (itr == NULL) delayedQ_root = node;
+            if (itr ->priority >= 8) delayedQ_root = node;
             else itr->next = node;
-            free(running);
         }
         else state = -1; //memory allocation error
     }
-
+		free(running);
     return state;
 }
 
 int Dispatch()
 {
     int state = 0;
-    if (readyQ_root != NULL)
+    if (readyQ_root -> priority < 8)
     {
         running = readyQ_root;
-        readyQ_root = readyQ_root->next;
+				
+				if (readyQ_root->next == NULL || readyQ_root->next->priority > 8 ) 
+				{
+					readyQ_root = NULL;
+					free(readyQ_root);
+					
+				}
+				else 
+				{
+					readyQ_root = readyQ_root->next;
+				}
         ((void(*)())running->task_ptr)();
         free(running);
 
@@ -105,3 +114,31 @@ int Dispatch()
     return state;
 }
 
+int decrementAll(int counter)
+{
+	volatile struct Qnode * itr;
+	itr = delayedQ_root;
+		while (itr ->priority <8)
+		{
+			itr ->sleep_time = itr ->sleep_time - counter;
+			counter = 0;
+			if (itr ->sleep_time == 0)
+			{
+				QueTask(itr -> task_ptr, itr->priority);
+				if (itr->next == NULL || itr->next->priority > 8 ) 
+				{
+					delayedQ_root = NULL;
+					free(delayedQ_root);
+					
+				}
+				else 
+				{
+					delayedQ_root = delayedQ_root->next;
+				}
+				itr = delayedQ_root;
+			}
+			else 
+				itr = itr -> next;
+		}
+		return counter;
+}
--- co_sched_API.h
+++ co_sched_API.h
@@ -1,4 +1,11 @@
 int Init();
-int QueTask(void (*task_ptr), unsigned int priority);
+int QueTask(volatile void (*task_ptr), unsigned int priority);
 int ReRunME(int delay);
 int Dispatch();
+int decrementAll(int counter);
+struct Qnode {
+    volatile int priority;
+    volatile int sleep_time;
+    volatile void (*task_ptr);
+    struct Qnode* next;
+};
