f37618c1cdba3e484195a09df4bcc3f1ccbb98c9
Mahfouz-z <43938352+Mahfouz-z@users.noreply.github.com>
2021-04-10 15:03:50 +0200

    Update co_sched_API.h

--- co_sched_API.h
+++ co_sched_API.h
@@ -1,11 +1,5 @@
-int Init();
-int QueTask(volatile void (*task_ptr), unsigned int priority);
+int Init(void);
+int QueTask(void (*task_ptr), unsigned int priority);
 int ReRunME(int delay);
-int Dispatch();
-int decrementAll(int counter);
-struct Qnode {
-    volatile int priority;
-    volatile int sleep_time;
-    volatile void (*task_ptr);
-    struct Qnode* next;
-};
+int Dispatch(void);
+void coop_sched_tick(void);
