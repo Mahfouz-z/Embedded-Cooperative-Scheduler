403a08f021706aabf4d475dc4de9c6a1ff3a83df
Mahfouz-z <43938352+Mahfouz-z@users.noreply.github.com>
2021-04-10 15:03:14 +0200

    Update co_sched.h

--- co_sched.h
+++ co_sched.h
@@ -1,10 +1,10 @@
-//// A queue linked list node
-//#ifndef Qnode
-//#define Qnode
-//struct Qnode {
-//    int priority;
-//    int sleep_time;
-//    void (*task_ptr);
-//    struct Qnode* next;
-//};
-//#endif
+// A queue linked list node
+
+struct Qnode {
+    int priority;
+    int sleep_time;
+    void (*task_ptr);
+    struct Qnode* next;
+};
+
+int decrementAll(int counter);
