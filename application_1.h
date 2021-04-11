1afcc3b6c930e48a531afa0909c8eb62f4139c33
Mahfouz-z <43938352+Mahfouz-z@users.noreply.github.com>
2021-04-11 19:57:46 +0200

    Update application_1.h

--- application_1.h
+++ application_1.h
@@ -1,6 +1,5 @@
 void init_I2C_UART_task(void);
 float read_temprature(void);
-void led_toggle(void);
 void alarm_led_toggle_task(void);
 void alarm_led_off_task(void);
 void update_temprature_task(void);
