537eea816ec02d640025c4cea5ac0ef42fbf8c1a
hysamah <57324118+hysamah@users.noreply.github.com>
2021-04-11 21:32:09 +0200

    updated media

--- unit_tests/delayedQ_unit_test.c
+++ unit_tests/delayedQ_unit_test.c
@@ -1,271 +1,271 @@
-#include "main.h"
-#include "co_sched.h"
-#include "co_sched_API.h"
+//#include "main.h"
+//#include "co_sched.h"
+//#include "co_sched_API.h"
 
-UART_HandleTypeDef huart2;
-void SystemClock_Config(void);
-static void MX_GPIO_Init(void);
-static void MX_USART2_UART_Init(void);
+//UART_HandleTypeDef huart2;
+//void SystemClock_Config(void);
+//static void MX_GPIO_Init(void);
+//static void MX_USART2_UART_Init(void);
 
-void Task1(){
-	
-	for (int i=0; i<8; i++)
-	{	
-		HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_3);
-		HAL_Delay(250);
-	}
-    ReRunMe(15);
-}
+//void Task1(){
+//	
+//	for (int i=0; i<8; i++)
+//	{	
+//		HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_3);
+//		HAL_Delay(250);
+//	}
+//    ReRunMe(15);
+//}
 
-void Task2(){
-	
-	for (int i=0; i<8; i++)
-	{	
-		HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_3);
-		HAL_Delay(500);
-	}
-    ReRunMe(10);
-}
+//void Task2(){
+//	
+//	for (int i=0; i<8; i++)
+//	{	
+//		HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_3);
+//		HAL_Delay(500);
+//	}
+//    ReRunMe(10);
+//}
 
-void Task3(){
-	
-	for (short int i=0; i<8; i++)
-	{	
-		HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_3);
-		HAL_Delay(50);
-	}
-    ReRunMe(0);
-}
+//void Task3(){
+//	
+//	for (short int i=0; i<8; i++)
+//	{	
+//		HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_3);
+//		HAL_Delay(50);
+//	}
+//    ReRunMe(0);
+//}
 
-void Task4(){
-	
-	for (short int i=0; i<8; i++)
-	{	
-		HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_3);
-		HAL_Delay(300);
-	}
-}
-void Task5(){
-	
-	for (short int i=0; i<8; i++)
-	{	
-		HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_3);
-		HAL_Delay(75);
-	}
-}
+//void Task4(){
+//	
+//	for (short int i=0; i<8; i++)
+//	{	
+//		HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_3);
+//		HAL_Delay(300);
+//	}
+//}
+//void Task5(){
+//	
+//	for (short int i=0; i<8; i++)
+//	{	
+//		HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_3);
+//		HAL_Delay(75);
+//	}
+//}
 
-int main(void)
-{
-    HAL_Init();
-    SystemClock_Config();
+//int main(void)
+//{
+//    HAL_Init();
+//    SystemClock_Config();
 
-    /* Initialize all configured peripherals */
-    MX_GPIO_Init();
-    MX_USART2_UART_Init();
+//    /* Initialize all configured peripherals */
+//    MX_GPIO_Init();
+//    MX_USART2_UART_Init();
 
-    /* Infinite loop */
-    /* USER CODE BEGIN WHILE */
-    Init();
-    QueTask(Task5, 0);
-    QueTask(Task4, 0);
-	QueTask(Task3, 1);
-    QueTask(Task1, 0);
-    QueTask(Task2, 1);
-    
-    /*assuming correct readyQ priorites, the tasks should be performed in the following order at first:
-    Task5 -> Task4 -> Task1 -> Task3 -> Task2 
-    Each task blinks the on-board LED 4 times with different delays, assuming  correct readyQ they delays will be in this order:
-    75 -> 300 -> 250 -> 50 -> 500
-    Then on caling the ReRunMe inside tasks 1,2, and 3 with different delay times they should run in the following order:
-    Assuming correct DelayedQ sorting
-    Task1 -> Task3 -> Task2
-    This should have the LED blinking 4 times continuously with different delays for each set of blinks as follows:
-    250 -> 50 -> 500
-    */
+//    /* Infinite loop */
+//    /* USER CODE BEGIN WHILE */
+//    Init();
+//    QueTask(Task5, 0);
+//    QueTask(Task4, 0);
+//	QueTask(Task3, 1);
+//    QueTask(Task1, 0);
+//    QueTask(Task2, 1);
+//    
+//    /*assuming correct readyQ priorites, the tasks should be performed in the following order at first:
+//    Task5 -> Task4 -> Task1 -> Task3 -> Task2 
+//    Each task blinks the on-board LED 4 times with different delays, assuming  correct readyQ they delays will be in this order:
+//    75 -> 300 -> 250 -> 50 -> 500
+//    Then on caling the ReRunMe inside tasks 1,2, and 3 with different delay times they should run in the following order:
+//    Assuming correct DelayedQ sorting
+//    Task1 -> Task3 -> Task2
+//    This should have the LED blinking 4 times continuously with different delays for each set of blinks as follows:
+//    250 -> 50 -> 500
+//    */
 
-	
-    while (1)
-    {
-		Dispatch();
-    }
-	
-}
-
-
+//	
+//    while (1)
+//    {
+//		Dispatch();
+//    }
+//	
+//}
 
 
 
-void SystemClock_Config(void)
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
-  /** Configure LSE Drive Capability
-  */
-  HAL_PWR_EnableBkUpAccess();
-  __HAL_RCC_LSEDRIVE_CONFIG(RCC_LSEDRIVE_LOW);
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_LSE|RCC_OSCILLATORTYPE_MSI;
-  RCC_OscInitStruct.LSEState = RCC_LSE_ON;
-  RCC_OscInitStruct.MSIState = RCC_MSI_ON;
-  RCC_OscInitStruct.MSICalibrationValue = 0;
-  RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_6;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_MSI;
-  RCC_OscInitStruct.PLL.PLLM = 1;
-  RCC_OscInitStruct.PLL.PLLN = 40;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV7;
-  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
-  RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART2;
-  PeriphClkInit.Usart2ClockSelection = RCC_USART2CLKSOURCE_PCLK1;
-  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /** Configure the main internal regulator output voltage
-  */
-  if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /** Enable MSI Auto calibration
-  */
-  HAL_RCCEx_EnableMSIPLLMode();
-}
+//void SystemClock_Config(void)
+//{
+//  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+//  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+//  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
-/**
-  * @brief USART2 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_USART2_UART_Init(void)
-{
+//  /** Configure LSE Drive Capability
+//  */
+//  HAL_PWR_EnableBkUpAccess();
+//  __HAL_RCC_LSEDRIVE_CONFIG(RCC_LSEDRIVE_LOW);
+//  /** Initializes the RCC Oscillators according to the specified parameters
+//  * in the RCC_OscInitTypeDef structure.
+//  */
+//  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_LSE|RCC_OSCILLATORTYPE_MSI;
+//  RCC_OscInitStruct.LSEState = RCC_LSE_ON;
+//  RCC_OscInitStruct.MSIState = RCC_MSI_ON;
+//  RCC_OscInitStruct.MSICalibrationValue = 0;
+//  RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_6;
+//  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+//  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_MSI;
+//  RCC_OscInitStruct.PLL.PLLM = 1;
+//  RCC_OscInitStruct.PLL.PLLN = 40;
+//  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV7;
+//  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
+//  RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
+//  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+//  {
+//    Error_Handler();
+//  }
+//  /** Initializes the CPU, AHB and APB buses clocks
+//  */
+//  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+//                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+//  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+//  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+//  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+//  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  /* USER CODE BEGIN USART2_Init 0 */
+//  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
+//  {
+//    Error_Handler();
+//  }
+//  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART2;
+//  PeriphClkInit.Usart2ClockSelection = RCC_USART2CLKSOURCE_PCLK1;
+//  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+//  {
+//    Error_Handler();
+//  }
+//  /** Configure the main internal regulator output voltage
+//  */
+//  if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
+//  {
+//    Error_Handler();
+//  }
+//  /** Enable MSI Auto calibration
+//  */
+//  HAL_RCCEx_EnableMSIPLLMode();
+//}
 
-  /* USER CODE END USART2_Init 0 */
+///**
+//  * @brief USART2 Initialization Function
+//  * @param None
+//  * @retval None
+//  */
+//static void MX_USART2_UART_Init(void)
+//{
 
-  /* USER CODE BEGIN USART2_Init 1 */
+//  /* USER CODE BEGIN USART2_Init 0 */
 
-  /* USER CODE END USART2_Init 1 */
-  huart2.Instance = USART2;
-  huart2.Init.BaudRate = 9600;
-  huart2.Init.WordLength = UART_WORDLENGTH_8B;
-  huart2.Init.StopBits = UART_STOPBITS_1;
-  huart2.Init.Parity = UART_PARITY_NONE;
-  huart2.Init.Mode = UART_MODE_TX_RX;
-  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
-  huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
-  huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
-  if (HAL_UART_Init(&huart2) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN USART2_Init 2 */
+//  /* USER CODE END USART2_Init 0 */
 
-  /* USER CODE END USART2_Init 2 */
+//  /* USER CODE BEGIN USART2_Init 1 */
 
-}
+//  /* USER CODE END USART2_Init 1 */
+//  huart2.Instance = USART2;
+//  huart2.Init.BaudRate = 9600;
+//  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+//  huart2.Init.StopBits = UART_STOPBITS_1;
+//  huart2.Init.Parity = UART_PARITY_NONE;
+//  huart2.Init.Mode = UART_MODE_TX_RX;
+//  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+//  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+//  huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+//  huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+//  if (HAL_UART_Init(&huart2) != HAL_OK)
+//  {
+//    Error_Handler();
+//  }
+//  /* USER CODE BEGIN USART2_Init 2 */
 
-/**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_GPIO_Init(void)
-{
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
+//  /* USER CODE END USART2_Init 2 */
 
-  /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOC_CLK_ENABLE();
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
+//}
 
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, GPIO_PIN_RESET);
+///**
+//  * @brief GPIO Initialization Function
+//  * @param None
+//  * @retval None
+//  */
+//static void MX_GPIO_Init(void)
+//{
+//  GPIO_InitTypeDef GPIO_InitStruct = {0};
 
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3, GPIO_PIN_RESET);
+//  /* GPIO Ports Clock Enable */
+//  __HAL_RCC_GPIOC_CLK_ENABLE();
+//  __HAL_RCC_GPIOA_CLK_ENABLE();
+//  __HAL_RCC_GPIOB_CLK_ENABLE();
 
-  /*Configure GPIO pin : PA8 */
-  GPIO_InitStruct.Pin = GPIO_PIN_8;
-  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+//  /*Configure GPIO pin Output Level */
+//  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, GPIO_PIN_RESET);
 
-  /*Configure GPIO pin : PA9 */
-  GPIO_InitStruct.Pin = GPIO_PIN_9;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+//  /*Configure GPIO pin Output Level */
+//  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3, GPIO_PIN_RESET);
 
-  /*Configure GPIO pin : PB3 */
-  GPIO_InitStruct.Pin = GPIO_PIN_3;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_PULLUP;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+//  /*Configure GPIO pin : PA8 */
+//  GPIO_InitStruct.Pin = GPIO_PIN_8;
+//  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+//  GPIO_InitStruct.Pull = GPIO_NOPULL;
+//  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-}
+//  /*Configure GPIO pin : PA9 */
+//  GPIO_InitStruct.Pin = GPIO_PIN_9;
+//  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+//  GPIO_InitStruct.Pull = GPIO_NOPULL;
+//  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+//  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-/* USER CODE BEGIN 4 */
+//  /*Configure GPIO pin : PB3 */
+//  GPIO_InitStruct.Pin = GPIO_PIN_3;
+//  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+//  GPIO_InitStruct.Pull = GPIO_PULLUP;
+//  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+//  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-/* USER CODE END 4 */
+//}
 
-/**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
-  /* USER CODE END Error_Handler_Debug */
-}
+///* USER CODE BEGIN 4 */
 
-#ifdef  USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t *file, uint32_t line)
-{
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
-}
-#endif /* USE_FULL_ASSERT */
+///* USER CODE END 4 */
 
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
+///**
+//  * @brief  This function is executed in case of error occurrence.
+//  * @retval None
+//  */
+//void Error_Handler(void)
+//{
+//  /* USER CODE BEGIN Error_Handler_Debug */
+//  /* User can add his own implementation to report the HAL error return state */
+//  __disable_irq();
+//  while (1)
+//  {
+//  }
+//  /* USER CODE END Error_Handler_Debug */
+//}
+
+//#ifdef  USE_FULL_ASSERT
+///**
+//  * @brief  Reports the name of the source file and the source line number
+//  *         where the assert_param error has occurred.
+//  * @param  file: pointer to the source file name
+//  * @param  line: assert_param error line source number
+//  * @retval None
+//  */
+//void assert_failed(uint8_t *file, uint32_t line)
+//{
+//  /* USER CODE BEGIN 6 */
+//  /* User can add his own implementation to report the file name and line number,
+//     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+//  /* USER CODE END 6 */
+//}
+//#endif /* USE_FULL_ASSERT */
+
+///************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
 
